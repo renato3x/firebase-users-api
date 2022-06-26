fd74d53f00c273c63ba3fd09cb962a5e6b468d08
renato3x <pereirarenato21@gmail.com>
2022-06-26 15:13:45 -0300

    users: save a new user in firebase with uid

--- src/firebase/models/User.js
+++ src/firebase/models/User.js
@@ -1,15 +1,17 @@
 const { getFirestore } = require('firebase-admin/firestore')
 const firebaseApp = require('../firebaseApp')
+const { v4: uid } = require('uuid')
 
 const usersCollection = getFirestore(firebaseApp).collection('users')
 
 module.exports = class User {
   static async insert(u) {
-    const data = await usersCollection.add(u)
     const user = {
-      ...u,
-      id: data.id
+      uid: uid(),
+      ...u
     }
+    
+    await usersCollection.add(user)
 
     return user
   }
