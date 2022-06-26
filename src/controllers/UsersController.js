53071c452fe723ea8020f508ce40d915ea316a77
renato3x <pereirarenato21@gmail.com>
2022-06-26 16:43:31 -0300

    app: get file in request

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,14 @@
 const UsersService = require('../services/UsersService')
+const { request: req, response: res } = require('express')
 
 module.exports = class UsersController {
-  static async index(request, response) {
+  static async index(_ = req, response = res) {
     const users = await UsersService.getAll()
 
     return response.status(200).json(users)
   }
 
-  static async findById(request, response) {
+  static async findById(request = req, response = res) {
     const user = await UsersService.findById(request.params.id)
 
     if (!user) {
@@ -19,13 +20,15 @@ module.exports = class UsersController {
     return response.status(200).json(user)
   }
 
-  static async create(request, response) {
+  static async create(request = req, response = res) {
     const {
       firstName,
       lastName,
       email,
       username
     } = request.body
+
+    console.log(request.file)
 
     try {
       const newUser = await UsersService.save({ firstName, lastName, email, username })
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const router = Router()
+const multer = require('multer')()
 
 const UsersController = require('../controllers/UsersController')
 
 router.get('/users', UsersController.index)
 router.get('/users/:id', UsersController.findById)
-router.post('/users', UsersController.create)
+router.post('/users', multer.single('profile'), UsersController.create)
 
 module.exports = router
