9d5819a5585011f88cb830a237d9b430780b66ed
renato3x <pereirarenato21@gmail.com>
2022-06-26 12:28:18 -0300

    firebase: user model

---
+++ src/database/models/User.js
@@ -0,0 +1,37 @@
+const { getFirestore, Timestamp } = require('firebase-admin/firestore')
+const { firebaseApp } = require('../../app')
+
+const usersCollection = getFirestore(firebaseApp).collection('users')
+
+module.exports = class User {
+  constructor(
+    firstName,
+    lastName,
+    username,
+    email,
+    username
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.email = email;
+    this.username = username;
+    this.createdAt = Timestamp.fromMillis();
+    this.updatedAt = Timestamp.fromMillis();
+  }
+
+  async save() {
+    const data = await usersCollection.add(this.generate())
+
+    return data
+  }
+
+  generate() {
+    const user = { ...this }
+
+    delete user.save
+    delete user.generate
+
+    return user
+  }
+}
